5741c50823b25038cbd258a633dff98965291c27
Cristian <41880357+cgutierrez21@users.noreply.github.com>
2024-03-16 22:52:11 -0500

    Update 2024-03-16 update readme

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Monkey Files
 #### File Explorer the way I want it.
+> Keep things as simple as possible and have what I need. Do not think ahead and add when needs come.
 ### What I Want
-1. Create new directory
-2. Create a file
-3. Search for a file or folder 
+1. Search for a file or folder 
+2. Rename files/directories
+3. Check for symlinks
+4. Create symlinks
 ### What's Been Implemented
 1. Display what's in a directory
 2. Return contents of a directory
 3. Get new path from a string
+4. Create new directory
 ### What I might change
-1. Return a vector of PathBufs instead of Strings? (WBI 2)
+1. 
 ## When I think the main file explorer functionality is complete
 1. Start moving towards gui
 2. Start looking at access to local network
--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,19 @@ fn new_path(given_path: &String) -> path::PathBuf {
     updated_path
 }
 
+fn create_directory(path: &path::PathBuf) {
+    // TODO: ask for new directory name
+    let directory_name = "New_Directory";
+    let _ = fs::create_dir(path.join(&directory_name));
+}
+
+fn create_goto_directory(path: &path::PathBuf) -> path::PathBuf {
+    // TODO: ask for new directory name
+    let directory_name = "New_Directory2";
+    let _ = fs::create_dir(path.join(&directory_name));
+    path.join(directory_name)
+}
+
 fn main() {
     // setting up home directory
     let home_directory: path::PathBuf;
@@ -60,20 +73,16 @@ fn main() {
     // setting up current directory
     let mut current_location = env::current_dir().unwrap();
 
-    // display content in current directory
+    // NOTE: Display content in current directory
     println!("Displaying the content of {}:", current_location.file_name().unwrap().to_str().unwrap());
     let _ = dir_content(&current_location);
 
     println!("\n======================================");
 
-    // vector with content of current directory
+    // NOTE: Vector with content of current directory
     let mut content_list = Vec::new();
     match get_content(&current_location) {
         Ok(content) => {
-            /* for path in content {
-                let current_file = path.file_name().unwrap().to_str().unwrap().to_string();
-                content_list.push(current_file)
-            } */
             content_list = content;
         }
         Err(err) => eprintln!("Error reading directory: {}", err),
@@ -84,9 +93,8 @@ fn main() {
             .to_lowercase()
             .cmp(&b.to_str().unwrap().to_lowercase())
     });
-    // content_list.sort();
 
-    // display those files/directories from vector
+    // NOTE: Display those files/directories from vector
     println!("Displaying the content of {}:", current_location.file_name().unwrap().to_str().unwrap());
     println!("\nDirectory contents:");
     for file in &content_list {
@@ -99,7 +107,8 @@ fn main() {
 
     println!("\n======================================");
 
-    // go into one of the folders and display whats inside (controlled for now using Pictures directory)
+    // NOTE: Go into one of the folders and display whats inside (controlled for now using Pictures directory)
+
     /* current_location = new_path(&content_list[2].to_str().unwrap().to_string());
     println!(
         "Files in {} directory:",
@@ -114,8 +123,24 @@ fn main() {
 
     println!("\n======================================");
 
-    // current_location.pop();
-    current_location = "~/".into();
+    // NOTE: Go up a directory
+
+    /* current_location.pop();
+    println!(
+        "Now in the {} directory:",
+        current_location
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string()
+    );
+    let _ = dir_content(&current_location); */
+    
+    // NOTE: Create directory
+    let _ = create_directory(&current_location);
+    // NOTE: Create directory and go into it after creation
+    current_location = create_goto_directory(&current_location);
     println!(
         "Now in the {} directory:",
         current_location
